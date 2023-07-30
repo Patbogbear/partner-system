b85fc6cfcea24098e3a013ce90430482702d8890
patrick <marqurse258@gmail.com>
2023-07-30 13:20:10 +0800

    new udpate

--- client/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp,VueElement } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-//import axios from './http'
-
-
-createApp(App).use(store).use(router).mount('#app')
