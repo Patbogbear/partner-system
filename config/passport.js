cca97fe5fdb29f5fda45a95084611d05bbb223c2
patrick <marqurse258@gmail.com>
2023-12-15 20:15:48 +0800

    security key issues

--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,7 @@ const JwtStrategy = require('passport-jwt').Strategy,
 ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
-const keys = require("./keys")
+const keys = process.env.SECRET_OR_KEY
 
 
 const opts = {}
